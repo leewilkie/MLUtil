7b42d15bd31cce03e4ebc5bebfcb749431a3e20b
Lee Wilkie <contact@lee-wilkie.com>
2021-03-14 21:19:03 +0000

    Initial commit

---
+++ gen_label_csv.py
@@ -0,0 +1,59 @@
+# based on https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
+
+# assumes running from TF folder models/research/object_detection
+
+
+import os
+import glob
+import argparse
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_names = ['filename', 'width', 'height',
+                    'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_names)
+    return xml_df
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-o', '--outfile', help='Filename for output csv.', required=True)
+
+    parser.add_argument('-f', '--folder',
+                        help='Folder containing images and xml label files (default: current)')
+
+    args = parser.parse_args()
+
+    folder = args.folder
+
+    if folder == None:
+        folder = os.getcwd()
+
+    xml_df = xml_to_csv(folder)
+    if xml_df.shape[0] > 0:
+        xml_df.to_csv(args.outfile, index=None)
+        print('Successfully converted xml to csv.')
+    else:
+        print('Nothing to convert.')
+
+
+main()
---
+++ gen_label_maps.py
@@ -0,0 +1,90 @@
+import csv
+import argparse
+
+
+def from_txt():
+    with open("labels.txt", "rt") as text_file:
+        pbtxt = []
+
+        # fill pbtxt array from file lines
+        lines = text_file.readlines()
+        for line in lines:
+            # remove newline
+            line = line.replace('\n', '')
+            # split on colon
+            vals = line.split(':')
+            pbtxt.append("item {\n")
+            pbtxt.append("  id: {}\n".format(vals[0]))
+            pbtxt.append("  name: {}\n".format(vals[1]))
+            pbtxt.append("}\n")
+
+        print(pbtxt)
+
+        return pbtxt
+
+
+def from_csv(csv_file, labels):
+    with open(csv_file, 'rt', newline='') as in_file:
+
+        # load the csv into a list and remove the first (header) row
+        reader = csv.reader(in_file)
+        rows = list(reader)
+        rows.pop(0)
+
+        # iterate all rows and create set of unique label names
+        for row in rows:
+            labels.add(row[3])
+
+
+def write_list(out_file, labels):
+    lines = []
+    label_id = 1
+    for label_name in labels:
+        lines.append("{},{}\n".format(label_id, label_name))
+        label_id += 1
+
+    with open(out_file, "wt") as out_file:
+        out_file.writelines(lines)
+
+
+def write_pbtxt(out_file, labels):
+    lines = []
+    label_id = 1
+    for label_name in labels:
+        lines.append("item {\n")
+        lines.append("  id: {}\n".format(label_id))
+        lines.append("  name: {}\n".format(label_name))
+        lines.append("}\n")
+        label_id += 1
+
+    with open(out_file, "wt") as out_file:
+        out_file.writelines(lines)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--infiles', nargs='+',
+                        help='<Required> List of CSV files to extract labels from.', required=True)
+
+    parser.add_argument('-op', '--outpbfile', default='labelmap.pbtxt',
+                        help='Output pbtxt filename (default: labelmap.pbtxt).')
+
+    parser.add_argument('-ol', '--outlistfile', default='labelmap.txt',
+                        help='Output pbtxt filename (default: labelmap.txt).')
+
+    args = parser.parse_args()
+
+    # print(args.infiles)
+
+    # read labels from each csv
+    labels = set()
+    for file in args.infiles:
+        from_csv(file, labels)
+
+    write_list(args.outlistfile, labels)
+
+    write_pbtxt(args.outpbfile, labels)
+
+
+main()
